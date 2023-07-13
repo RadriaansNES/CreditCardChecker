5dc227e54c6872deb954c05c0d7d3941c1f1b433
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-07-13 10:53:08 -0400

    Company verifcation added.

--- main.js
+++ main.js
@@ -13,11 +13,11 @@ const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5]
 const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4]
 
 // Can be either valid or invalid
-const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4] //false 
-const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9] //true 
-const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3] //false
-const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3] //false 
-const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3] //true 
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4]
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9] 
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3] 
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3] 
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3] 
 
 // An array of all the arrays above
 const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
@@ -51,10 +51,10 @@ function ValidateCred(parray) {
     else { return false }
 }
 
-function findInvalidCards(NestedArray) {
+function findInvalidCards(NestedArray) { 
     let newray = [];
     for (let i = 0; i < NestedArray.length; i++) {
-        if (ValidateCred(NestedArray[i]) === false){
+        if (ValidateCred(NestedArray[i]) === false){  //if invalid pass to a new array, return
             newray.push(NestedArray[i]);
         }
     }
@@ -63,4 +63,43 @@ function findInvalidCards(NestedArray) {
 
 console.log(findInvalidCards(batch));
 
+function idInvalidCardCompanies(NestedArray) { //would be a nested array of invalid numbers, but for testing purposes this is how its outlined
+    let newray = findInvalidCards(NestedArray); //if passing invalid array, you'd pass the invalid array and remove line 67
+    let kray = [0,0,0,0,0]; //add array since they want the company check in array form (not blank since were using as counter)
 
+    for (let i = 0; i < newray.length; i++) { //cycle through nested
+        for (let j = 0; j < 1; j++) {
+            if (newray[i][j] === 3) { //check first digit of invalid numbers
+                kray[0] += 1;
+            }
+            else if (newray[i][j] === 4) {
+                kray[1] += 1;
+            }
+            else if (newray[i][j] === 5) {
+                kray[2] += 1;
+            }
+            else if (newray[i][j] === 6) {
+                kray[3] += 1;
+            }
+            else {kray[4] += 1;}
+        }
+    }
+    if (kray[0] > 0) { //print company name to array
+        kray[0] = 'Amex';
+    }
+    if (kray[1] > 0) { 
+        kray[1] = 'Visa';
+    }
+    if (kray[2] > 0) { 
+        kray[2] = 'Mastercard';
+    }
+    if (kray[3] > 0) { 
+        kray[3] = 'Discover';
+    }
+    if (kray[4] > 0) { 
+        kray[4] = 'Unknown companies found';
+    }
+    return kray;
+}
+
+console.log(idInvalidCardCompanies(batch));

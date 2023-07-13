51f8e80384a669c880b35b8f67295e168b0de14f
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-07-13 09:40:57 -0400

    nested array check.

--- main.js
+++ main.js
@@ -13,11 +13,11 @@ const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5]
 const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4]
 
 // Can be either valid or invalid
-const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4]
-const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9]
-const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3]
-const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
-const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4] //false 
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9] //true 
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3] //false
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3] //false 
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3] //true 
 
 // An array of all the arrays above
 const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
@@ -25,7 +25,7 @@ const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, inval
 // Add your functions below:
 
 function ValidateCred(array) {
-    for (i = array.length; i >= 1; i--) {
+    for (let i = array.length; i >= 1; i--) {
         if (i < array.length && array.length % 2 === 1) { //odd array number check
             if (i % 2 === 0) { //double every other digit 
                 array[i - 1] = array[i - 1] * 2;
@@ -45,16 +45,21 @@ function ValidateCred(array) {
     }
     const sum = array.reduce((partialSum, a) => partialSum + a, 0); //sum across array
     if (sum % 10 === 0) {
-        return true
+        return true //valid
     }
     else { return false }
 }
 
-function name(params) {
-    
+function findInvalidCards(NestedArray) {
+    var newray = [];
+    for (let i = 0; i < NestedArray.length; i++) {
+        if (ValidateCred(NestedArray[i]) === false){
+            newray.push(NestedArray[i]);
+        }
+    }
+    return newray;
 }
 
-
-
+console.log(findInvalidCards(batch));
 
 
